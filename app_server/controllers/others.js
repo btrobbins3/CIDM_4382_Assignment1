63ba5380d91c621e2ff3617e56393ff5c80d9c46
Robbins <brobbins@wtamu.edu>
2019-09-23 12:16:08 -0500

    second modification

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -3,7 +3,7 @@ const about = (req, res) => {
   res.render('generic-text',
     {
       title: 'About Loc8r',
-      content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan. Nullam sit amet interdum magna. Morbi quis faucibus nisi. Vestibulum mollis purus quis eros adipiscing tristique. Proin posuere semper tellus, id placerat augue dapibus ornare. Aenean leo metus, tempus in nisl eget, accumsan interdum dui. Pellentesque sollicitudin volutpat ullamcorper.'
+      content: 'Looking for the best pizza near you? Loc8r helps you find pizza joints without you having to waste you money to find the good ones. Let Loc8r help you find the place you\'re looking for.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan. Nullam sit amet interdum magna. Morbi quis faucibus nisi. Vestibulum mollis purus quis eros adipiscing tristique. Proin posuere semper tellus, id placerat augue dapibus ornare. Aenean leo metus, tempus in nisl eget, accumsan interdum dui. Pellentesque sollicitudin volutpat ullamcorper.'
     }
   );
 };
